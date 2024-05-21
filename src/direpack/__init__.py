939adb801a44879a3152bd14bbdc1d320bdbe8bc
leleist <141621096+leleist@users.noreply.github.com>
2024-05-21 14:48:30 +0200

    uncommenting ppdire to negate import errors __init__.py
    this is just to make snipls run on a server where ppdire dependencies cannot be installed

--- src/direpack/__init__.py
+++ src/direpack/__init__.py
@@ -29,12 +29,12 @@ from .sprm.sprm import sprm
 from .sprm.snipls import snipls
 from .sprm.rm import rm
 from .cross_validation._cv_support_functions import robust_loss
-from .ppdire.ppdire import ppdire
-from .ppdire.capi import capi
-from .dicomo.dicomo import dicomo
-from .sudire.sudire import sudire, estimate_structural_dim
-from .plot.sudire_plot import sudire_plot
-from .plot.ppdire_plot import ppdire_plot
+#from .ppdire.ppdire import ppdire
+#from .ppdire.capi import capi
+f#rom .dicomo.dicomo import dicomo
+#from .sudire.sudire import sudire, estimate_structural_dim
+#from .plot.sudire_plot import sudire_plot
+#from .plot.ppdire_plot import ppdire_plot
 from .plot.sprm_plot import sprm_plot, sprm_plot_cv
-from .ipopt_temp.ipopt_wrapper import minimize_ipopt
-from .ipopt_temp.jacobian import *
+#from .ipopt_temp.ipopt_wrapper import minimize_ipopt
+#from .ipopt_temp.jacobian import *
